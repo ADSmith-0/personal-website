0f27ad78c4cea21acf29e35382fc552f88ff78f9
ADSmith-0 <adam00199382818@gmail.com>
2021-07-08 17:09:48 +0100

    init commit

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import styles from '../styles/header.module.css';
+
+export default function header() {
+    return (
+        <nav id={styles.container}>
+            <Link href="/"><a className={styles.link}>Home</a></Link>
+            <Link href="/projects"><a className={styles.link}>Projects</a></Link>
+            <Link href="/blog"><a className={styles.link}>Blog</a></Link>
+            <Link href="/about"><a className={styles.link}>About</a></Link>
+        </nav>
+    )
+}
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+title: 'test'
+author: 'adam'
+date: '2020-01-01'
+published: true
+---
+# Test
+
+lorem ipsum
