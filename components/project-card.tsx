34ba04e53445060c9a20efd104b1c2e7651049ab
ADSmith-0 <adam00199382818@gmail.com>
2021-07-18 02:26:21 +0100

    update: added link to project-card and updated ProjectType to add link

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 export default function projectCard({ project:{slug, frontmatter}}:{project: ProjectType}) {
     return (
         <div>
-            <Image 
-                src={frontmatter.cover_image}
-                height={300}
-                width={420}
-                alt={frontmatter.title+" cover image"}
-            />
-            <h3>{frontmatter.title}</h3>
-            <p>{frontmatter.status}</p>
-            <p>{frontmatter.live}</p>
+            <a href={frontmatter.link}>
+                <Image 
+                    src={frontmatter.cover_image}
+                    height={300}
+                    width={420}
+                    alt={frontmatter.title+" cover image"}
+                />
+                <h3>{frontmatter.title}</h3>
+                <p>{frontmatter.status}</p>
+                <p>{frontmatter.live}</p>
+            </a>
         </div>
     )
 }
--- types/project.tsx
+++ types/project.tsx
@@ -1,6 +1,7 @@
 export type ProjectType = {
     slug: string,
     frontmatter: {
+        link: string,
         title: string,
         cover_image: string,
         status: string,
