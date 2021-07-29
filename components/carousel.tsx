f9ac418395cb1abdfcff7774081873710b3eb30b
ADSmith-0 <adam00199382818@gmail.com>
2021-07-29 15:48:48 +0100

    update: changed carousel to have scrolling

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,23 +1,23 @@
 import styles from '../styles/carousel.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 export default function Carousel({ children }:any){
-    const [ currentlySelected, setCurrentlySelected ] = useState(0);
-    const updateCurrentlySelected:any = (number:number) => {
-        if(currentlySelected+number >= 0 && currentlySelected+number < children.length){
-            setCurrentlySelected(currentlySelected+number);
-            // document.getElementById(styles.carousel);
-        }
-    }
+    const [ scrollAmount, setScrollAmount ] = useState(0);
+    const scrollLeft = () => setScrollAmount(Math.max(0, scrollAmount - 460));
+    const scrollRight = () => setScrollAmount(Math.min(scrollAmount + 460, ((children.length-2)*460)));
+    useEffect(() => {
+        document.getElementById(styles.carousel)?.scroll({
+            left: scrollAmount, 
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, [scrollAmount]);
     return (
-        <>
+        <div id={styles.container}>
+            <button id={styles.left_btn} className={styles.btn} onClick={scrollLeft}>➜</button>
             <div id={styles.carousel}>
-                <button id={styles.left_btn} className={styles.btn} onClick={()=>updateCurrentlySelected(-1)}>➜</button>
-                { children.filter((child:any, index:number) => {
-                    if(index == currentlySelected) return child;
-                }) }
-                <button id={styles.right_btn} className={styles.btn} onClick={()=>updateCurrentlySelected(1)}>➜</button>
+                { children }
             </div>
-            <div style={{height: "1000px"}}></div>
-        </>
+            <button id={styles.right_btn} className={styles.btn} onClick={scrollRight}>➜</button>
+        </div>
     )
 }
