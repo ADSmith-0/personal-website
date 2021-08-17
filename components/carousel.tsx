e6b132a446c03306e78188d9061ec7e6887d2b3d
ADSmith-0 <adam00199382818@gmail.com>
2021-08-17 11:12:30 +0100

    update: refactored and fixed carousel scrolling

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -2,25 +2,19 @@ import styles from '../styles/carousel.module.css';
 import { useState, useEffect, useRef } from 'react';
 export default function Carousel({ children }:any){
     const carouselRef = useRef(null);
-    const [cardWidth, setCardWidth] = useState(0);
-    const [ scrollAmount, setScrollAmount ] = useState(0);
-    const scrollLeft = () => setScrollAmount(Math.max(0, scrollAmount - cardWidth));
-    const scrollRight = () => {
-        let displayNumber = (window.innerWidth < 500) ? 1 : ((window.innerWidth < 1300)? 2 : 3); 
-        setScrollAmount(Math.min(scrollAmount + cardWidth, ((children.length-displayNumber)*cardWidth)));
-    }
+    const [ position, setPosition ] = useState(0);
+    //@ts-ignore
+    const scrollLeft = () => setPosition(Math.max(0, (carouselRef.current.scrollLeft - carouselRef.current.clientWidth)));
+    //@ts-ignore
+    const scrollRight = () => setPosition(Math.min((carouselRef.current.scrollLeft + carouselRef.current.clientWidth), carouselRef.current.scrollWidth));
     useEffect(() => {
-        let cardWidth = (window.innerWidth < 500) ? 0.85*window.innerWidth+10 : ((window.innerWidth < 1300) ? 460 : 910);
-        setCardWidth(cardWidth);
-    }, []);
-    useEffect(() => {
-        // @ts-ignore 
-        carouselRef.current.scrollTo({
+        //@ts-ignore
+        carouselRef.current.scroll({
             top: 0,
-            left: scrollAmount, 
+            left: position,
             behavior: 'smooth'
         });
-    }, [scrollAmount, carouselRef]);
+    }, [ position ]);
     return (
         <div className={styles.container}>
             <button id={styles.left_btn} className={styles.btn} onClick={scrollLeft}>➜</button>
